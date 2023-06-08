a4aeb46bc0f3a66c7722c08ce46944c436522269
Pooyan <thepooyan@gmail.com>
2023-06-08 14:32:02 +0330

    new comment register js add

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,23 @@ if (kidsComment) {
     let registerForm = kidsComment.query('form');
     let commentBox = registerForm.query('textarea');
 
+    commentBox.onkeydown = () => {
+        commentBox.classList.remove('error');
+    }
+    //submit new comment
     registerForm.onsubmit = e => {
         e.preventDefault();
 
-        console.log("comment to be saved: ", commentBox.value)
-        commentBox.value = '';
+        if (!commentBox.value) {
+            callModal.fail('لطفا نظر خود را وارد کنید');
+            commentBox.classList.add('error');
+            return
+        }
+
+        console.log("comment to be saved: ", commentBox.value);
+        callModal.success('نظر شما با موفقیت ثبت شد و پس از بازرسی ادمین نمایش داده میشود').then(()=>{
+            commentBox.value = '';
+        })
     }
 
     //reply button event
