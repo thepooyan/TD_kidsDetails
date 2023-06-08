ff87b053d0affb03c24e52cce8daa7f2d7f33031
Pooyan <thepooyan@gmail.com>
2023-06-08 14:14:44 +0330

    comments phase one

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
+const kidsComment = dc.id("kids_comments");
+if (kidsComment) {
+    let registerForm = kidsComment.query('form');
+    registerForm.onsubmit = e => {
+        e.preventDefault();
+        alert('register')
+    }
 
+    //reply button event
+    let replys = kidsComment.queries('.comment button');
+
+    replys.forEach(button => {
+        button.onclick = () => {
+            alert('reply')
+        }
+    });
+}
